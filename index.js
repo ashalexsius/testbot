2e70dc597031b7e57df27bfae0159612827412a1
ashalexsius <30518515+ashalexsius@users.noreply.github.com>
2017-07-28 13:05:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,25 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'webhook-echo-sample'
+        sendMessage (sender, speech)
     });
+  
+    
 });
 
+  
+    function sendMessage(sender,text)
+    {
+        "greeting":[
+    {
+      "locale":"default",
+      "text":"Hello!"
+    }, {
+      "locale":"en_US",
+      "text":"Timeless apparel for the masses."
+    }
+  ] 
+    }
 
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
