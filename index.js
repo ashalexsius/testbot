3419bde1526db707ac0b1a13fa41a86d3867d2c3
ashalexsius <30518515+ashalexsius@users.noreply.github.com>
2017-08-06 18:00:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,24 @@ restService.post('/echo', function(req, res) {
     });
 });
 
+restService.post('/fb', function(req, res) {
+     "message":{
+    "text":"Pick a color:",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Red",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+      },
+      {
+        "content_type":"text",
+        "title":"Green",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      }
+    ]
+  }
+});
+
 restService.post('/slack-test', function(req, res) {
 
     var slack_message = {
